d9de992edf23cfea6175c59f720d4d925c42e6cd
uaijoaopaulo <joao.paulo@hubsell.com.br>
2025-06-08 20:38:16 -0300

    Correção criação mongo composer

--- mongo-init/00-init-user.js
+++ mongo-init/00-init-user.js
@@ -4,6 +4,7 @@ db.createUser({
     user: 'usrVehicleRegistry',
     pwd: '8xNpyq8dtv',
     roles: [
+        { role: 'readWrite', db: 'vehicleregistry' },
         { role: 'dbAdmin', db: 'vehicleregistry' }
     ]
 });
